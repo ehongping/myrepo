0fd197998a9e72f60df161126bcf57f13f362baa
quan_hping@heetian.com <quan_hping@heetian.com>
2022-08-17 14:10:13 +0800

    docker-as-transfer

--- glibc4alpine.sh
+++ glibc4alpine.sh
@@ -1,3 +1,10 @@
     wget -O /download/glibc-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk 
     wget -O /download/glibc-bin-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-bin-2.35-r0.apk 
     wget -O /download/glibc-i18n-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-i18n-2.35-r0.apk 
+
+
+docker run -d --name busybox sleep infinity
+docker cp download busybox:/
+docker commit busybox ${{secrets.DOCKERHUB_REPONAME}}/busybox-glibc
+docker push ${{secrets.DOCKERHUB_REPONAME}}/busybox-glibc
+
