01b11568bd32d762e2b23479ef76511199f1471f
quan_hping@heetian.com <quan_hping@heetian.com>
2022-08-17 14:21:58 +0800

    docker-as-transfer to workflows

--- glibc4alpine.sh
+++ glibc4alpine.sh
@@ -1,7 +1,7 @@
-mkdir -p /download    
-wget -O /download/glibc-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk 
-    wget -O /download/glibc-bin-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-bin-2.35-r0.apk 
-    wget -O /download/glibc-i18n-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-i18n-2.35-r0.apk 
+mkdir -p download    
+wget -O download/glibc-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-2.35-r0.apk 
+    wget -O download/glibc-bin-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-bin-2.35-r0.apk 
+    wget -O download/glibc-i18n-2.35-r0.apk https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.35-r0/glibc-i18n-2.35-r0.apk 
 
 
 docker run -d --name busybox busybox sleep infinity
