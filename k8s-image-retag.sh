bec8ed9e7ac52e4ed8408453fcd86205775abf78
quan_hping@heetian.com <quan_hping@heetian.com>
2022-09-07 16:22:31 +0800

    k8s v1.25.0 image v5

--- k8s-image-retag.sh
+++ k8s-image-retag.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# docker pull
+awk -F: '{print $1,$2}'  k8s-image.txt|while read name tag ; do docker pull $name:$tag ;done
 # docker tag
 awk -F: '{print $1,$2}'  k8s-image.txt|while read name tag ; do docker tag $name:$tag ehongping/${name##*/}:$tag; done
 # docker push 
