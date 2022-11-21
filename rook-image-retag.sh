2576139133ea92aec5c4e0f691f0b9690fcfc866
quan_hping@heetian.com <quan_hping@heetian.com>
2022-11-21 14:42:30 +0800

    rokk image push by Version

--- rook-image-retag.sh
+++ rook-image-retag.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+VER=v1.10.6
+wget https://raw.githubusercontent.com/rook/rook/v1.10.6/deploy/examples/images.txt -O rook-images.txt
 # docker pull
-awk -F: '{print $1,$2}'  rook-image.txt|while read name tag ; do docker pull $name:$tag ;done
+awk -F: '{print $1,$2}'  rook-images.txt|while read name tag ; do docker pull $name:$tag ;done
 # docker tag
-awk -F: '{print $1,$2}'  rook-image.txt|while read name tag ; do docker tag $name:$tag ehongping/${name##*/}:$tag; done
+awk -F: '{print $1,$2}'  rook-images.txt|while read name tag ; do docker tag $name:$tag ehongping/${name##*/}:$tag; done
 # docker push 
 docker images|awk '/ehongping/{print $1,$2}'|while read name tag; do docker push  $name:$tag; done
