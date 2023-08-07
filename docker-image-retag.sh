c119bf99df2956b3ef732b1c1fa6c037cb965dbc
hp <hp@notebook.work.cs>
2023-08-07 13:35:14 +0800

    repull cadvisor

--- docker-image-retag.sh
+++ docker-image-retag.sh
@@ -2,5 +2,5 @@
 
 VERSION=v0.36.0
 docker pull gcr.io/cadvisor/cadvisor:$VERSION
-docker tag gcr.io/cadvisor/cadvisor:$VERSION ehongping/cadvisor/cadvisor:$VERSION
-docker push ehongping/cadvisor/cadvisor:$VERSION
+docker tag gcr.io/cadvisor/cadvisor:$VERSION ehongping/cadvisor:$VERSION
+docker push ehongping/cadvisor:$VERSION
