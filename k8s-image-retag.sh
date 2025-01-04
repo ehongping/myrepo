f52747a0e03743a740194fcd8eb21f010247c27f
grade.D <1158656377@qq.com>
2025-01-04 22:55:02 +0800

    hub.docker.com: cannot create subdir,exam: coredns/coredns

--- k8s-image-retag.sh
+++ k8s-image-retag.sh
@@ -2,7 +2,7 @@
 
 # docker pull
 awk -F: '{print $1,$2}'  k8s-image.txt|while read name tag ; do docker pull $name:$tag ;done
-# docker tag
-awk -F: '{print $1,$2}'  k8s-image.txt|while read name tag ; do docker tag $name:$tag ehongping/${name#*/}:$tag; done
+# docker tag,coredns/coredns denied: requested access to the resource is denied
+awk -F: '{print $1,$2}'  k8s-image.txt|while read name tag ; do docker tag $name:$tag ehongping/${name##*/}:$tag; done
 # docker push 
 docker images|awk '/ehongping/{print $1,$2}'|while read name tag; do docker push  $name:$tag; done
